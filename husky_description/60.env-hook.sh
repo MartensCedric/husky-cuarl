6ddbb5847dd73d3a6c84ac48d20759fae5641225
MartensCedric <cedricmartens98@gmail.com>
2020-07-06 14:10:08 -0400

    Corrected some paths

--- husky_description/60.env-hook.sh
+++ husky_description/60.env-hook.sh
@@ -1,1 +1,1 @@
-export HUSKY_DESCRIPTION=$(catkin_find --first-only --without-underlays husky_cuarl husky_description/urdf/custom_description.urdf.xacro 2>/dev/null)
+export HUSKY_DESCRIPTION=$(catkin_find --first-only --without-underlays husky_description urdf/custom_description.urdf.xacro 2>/dev/null)
--- husky_gazebo/60.env-hook.sh
+++ husky_gazebo/60.env-hook.sh
@@ -1,1 +1,1 @@
-export HUSKY_GAZEBO_DESCRIPTION=$(catkin_find --first-only --without-underlays husky_cuarl husky_gazebo/urdf/custom_description.urdf.xacro 2>/dev/null)
+export HUSKY_GAZEBO_DESCRIPTION=$(catkin_find --first-only --without-underlays husky_gazebo urdf/custom_description.urdf.xacro 2>/dev/null)
